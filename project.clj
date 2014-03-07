71978f35f69326fb2ef6e48a436099c2ab318ae9
razi <ssj.raziel@gmail.com>
2014-03-07 19:17:59 +0100

    Add uberjar settings

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
 (defproject raziblog "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sanity check"
+  :url "sc.razi.si"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [com.h2database/h2 "1.3.170"]
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.1.0"]]
+  :aot :all
+  :uberjar-name "raziblog-0.1.0-standalone.jar"
+  :min-lein-version "2.0.0"
   :main raziblog.handler
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler raziblog.handler/app}
