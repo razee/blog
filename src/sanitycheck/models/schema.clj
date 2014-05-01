1e272d3621f65dfb2417f10a90d1b17b9465b039
razi <ssj.raziel@gmail.com>
2014-05-01 12:55:59 +0200

    Deploy fails, try commenting out local db spec

--- src/sanitycheck/models/schema.clj
+++ src/sanitycheck/models/schema.clj
@@ -4,12 +4,12 @@
 (def db
    (or (System/getenv "DATABASE_URL")
               "postgresql://localhost:5432/sanitycheck"))
-(def dblocal
-  {:classname "org.postgresql.Driver" ; must be in classpath
-           :subprotocol "postgresql"
-           :subname "//localhost:5432/sanitycheck"
-           ; Any additional keys are passed to the driver
-           ; as driver-specific properties.
-           :user "razi"})
-          ; :password "secret"})
+;(def dblocal
+ ; {:classname "org.postgresql.Driver" ; must be in classpath
+  ;         :subprotocol "postgresql"
+   ;        :subname "//localhost:5432/sanitycheck"
+    ;       ; Any additional keys are passed to the driver
+     ;      ; as driver-specific properties.
+      ;     :user "razi"})
+       ;   ; :password "secret"})
 
