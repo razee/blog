3fe5be787c7d93a8aa78cd28061d319f98aee73d
razi <ssj.raziel@gmail.com>
2014-05-01 12:09:13 +0200

    Local db for testing purposes

--- src/sanitycheck/models/schema.clj
+++ src/sanitycheck/models/schema.clj
@@ -1,5 +1,15 @@
 (ns sanitycheck.models.schema)
 
+
 (def db
    (or (System/getenv "DATABASE_URL")
               "postgresql://localhost:5432/sanitycheck"))
+(def dblocal
+  {:classname "org.postgresql.Driver" ; must be in classpath
+           :subprotocol "postgresql"
+           :subname "//localhost:5432/sanitycheck"
+           ; Any additional keys are passed to the driver
+           ; as driver-specific properties.
+           :user "razi"})
+          ; :password "secret"})
+
