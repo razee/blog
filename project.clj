fe5dadc2e032fae29f736306967da468cdb07b8e
razi <ssj.raziel@gmail.com>
2014-05-18 22:00:21 +0200

    Add Clojurescript deps and config

--- project.clj
+++ project.clj
@@ -14,11 +14,24 @@
                  [clj-time "0.7.0"]; :exclusions [org.clojure/clojure]]
                  [ring/ring-anti-forgery "0.3.1" :exclusions [hiccup]]
                  [enlive "1.1.5"]
-                 [ring-basic-authentication "1.0.5"]]
-  :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
+                 [ring-basic-authentication "1.0.5"]
+                 [org.clojure/clojurescript "0.0-2202"]]
+  ;:repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
   :uberjar-name "sanitycheck-0.1.0-standalone.jar"
   :min-lein-version "2.0.0"
-  :main ^:skip-aot sanitycheck.handler
-  :plugins [[lein-ring "0.8.10"]]
+  :main  sanitycheck.handler
+  :aot :all
+  :plugins [[lein-ring "0.8.10"]
+            [lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+    :builds [{
+              :source-paths ["resources/public/cljs"]
+
+              :compiler {
+                :output-to "resources/public/cljs/"
+                :optimizations :whitespace
+                :pretty-print true}}]}
+  :hooks [leiningen.cljsbuild]
+  :jar true
   :ring {:handler sanitycheck.handler/app}
   :profiles {:uberjar {:aot :all}})
