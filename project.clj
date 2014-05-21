5969a5b68670daa7b71dc34c717a8b593a5010c4
razi <ssj.raziel@gmail.com>
2014-05-21 13:09:45 +0200

    Comment out Cljs hook to see if Heroku dyno wakes up faster

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
                 :output-to "resources/public/cljs/"
                 :optimizations :whitespace
                 :pretty-print true}}]}
-  :hooks [leiningen.cljsbuild]
+  ;:hooks [leiningen.cljsbuild]
   :jar true
   :ring {:handler sanitycheck.handler/app}
   :profiles {:uberjar {:aot :all}})
